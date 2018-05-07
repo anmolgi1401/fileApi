7b1cc178030e2c0bae3fe091c0c27d23ceca5d12
anmol gupta <anmolgi1401@gmail.com>
2018-05-07 18:59:40 +0530

    bug fixed

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var filesystem = require('./lib/index');
 
 filesystem.config('/home/anmol/Desktop/data',{isreated:false});
 
-filesystem.create ('anmol','anmol1','reandom string',(err)=>{
+filesystem.create ('anmol3','anmol7','reandom string',(err)=>{
 	console.log(err);
 })

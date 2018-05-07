e0b01a869828dbeebb545f8992b3e5afa993edfa
anmol gupta <anmolgi1401@gmail.com>
2018-05-07 20:39:18 +0530

    version patch

--- index.js
+++
@@ -1,7 +0,0 @@
-var filesystem = require('./lib/index');
-
-filesystem.config('/home/anmol/Desktop/data',{isreated:false});
-
-filesystem.create ('anmol4','anmol2','reandom string',(err)=>{
-	console.log(err);
-})
